81eb8c23dc1c1e85733b47f9fcecebbf4d618302
Jordan Morrison <jordanmorrison476@gmail.com>
2018-02-04 12:15:45 +0000

    Add 1

--- index.php
+++ index.php
@@ -1,2 +1,5 @@
 <?php include_once("homepage.html") ?>
-
+<?php include_once("CV/cv_page1.html") ?>
+<?php include_once("CV/cv_page2.html") ?>
+<?php include_once("Interests/sports.html") ?>
+<?php include_once("Interests/travel.html") ?>
