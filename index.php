871bda71940ab7594102afa079ef41dde3f17901
Jordan Morrison <jordanmorrison476@gmail.com>
2018-02-20 12:02:32 +0000

    Fixed z-index issue, Added Favicon_1, Formatted CV Pages and Interests

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 <?php include_once("CV/cv_page1.html") ?>
 <?php include_once("CV/cv_page2.html") ?>
 <?php include_once("Interests/sports.html") ?>
-<?php include_once("Interests/travel.html") ?>
+<?php include_once("Interests/travel.html") ?>   
